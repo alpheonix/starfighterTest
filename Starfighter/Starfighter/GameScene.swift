4e1fb08fdf7eb3297a6f18396bbe5421b408d0c9
arthur <arthurblanc@Mac-mini-de-Arthur.local>
2019-12-24 15:36:29 +0100

    premier

---
+++ Starfighter/Starfighter/GameScene.swift
@@ -0,0 +1,143 @@
+//
+//  GameScene.swift
+//  Starfighter
+//
+//  Created by Arthur BLANC on 24/12/2019.
+//  Copyright © 2019 Arthur BLANC. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene,SKPhysicsContactDelegate {
+    
+    var starfield:SKEmitterNode!
+    var scoreLabel:SKLabelNode!
+    var player:SKSpriteNode!
+    var torpilleNode: SKSpriteNode!
+    var score:Int = 0{
+     didSet {
+        scoreLabel.text = "Score: \(score)"
+        }
+    }
+    var possibleAliens = ["tie","falcon","starDestroyer"]
+    
+    var gameTimer: Timer!
+    let alienCategory:UInt32 = 0x1 << 1
+    let torpilleCategory:UInt32 = 0x1 << 0
+    override func didMove(to view: SKView) {
+        
+        starfield = SKEmitterNode(fileNamed: "Starfield")
+        starfield.position = CGPoint(x: 0, y: 1472)
+        starfield.advanceSimulationTime(10)
+        self.addChild(starfield)
+        starfield.zPosition = -1
+        
+        player = SKSpriteNode(imageNamed: "falcon")
+        
+        player.position = CGPoint(x: 0, y: -450)
+        
+        self.addChild(player)
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        
+        self.physicsWorld.contactDelegate = self
+        
+        scoreLabel = SKLabelNode(text: "Score: 0")
+        scoreLabel.position = CGPoint(x: 0, y: 0)
+        scoreLabel.fontName = "Zapfino"
+        scoreLabel.fontSize = 36
+        scoreLabel.color = UIColor.white
+        self.addChild(scoreLabel)
+        addAlien()
+        //gameTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(addAlien), userInfo: nil, repeats: true)
+        
+    
+}
+    
+   @objc func addAlien( ){
+    possibleAliens = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: possibleAliens) as! [String]
+    let alien = SKSpriteNode(imageNamed: possibleAliens[0])
+    alien.size = CGSize(width: alien.size.width/3, height: alien.size.width/3)
+    let randomPos = GKRandomDistribution(lowestValue: -300, highestValue: 300)
+    let position = CGFloat(randomPos.nextInt())
+    
+    alien.position = CGPoint(x: 0, y: 500)
+    alien.physicsBody = SKPhysicsBody(rectangleOf: alien.size)
+    alien.physicsBody?.isDynamic = true
+    
+    alien.physicsBody?.categoryBitMask = alienCategory
+    alien.physicsBody?.contactTestBitMask = torpilleCategory
+    alien.physicsBody?.collisionBitMask = 0
+    self.addChild(alien)
+    
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        fireTorpille()
+    }
+    
+    func fireTorpille(){
+        self.run(SKAction.playSoundFileNamed("torpedo.mp3", waitForCompletion: false))
+        
+        torpilleNode = SKSpriteNode(imageNamed: "laser")
+        torpilleNode.position = CGPoint(x: 0, y: -450)
+        torpilleNode.position.y += 5
+        
+
+        torpilleNode.physicsBody = SKPhysicsBody(rectangleOf: torpilleNode!.size)
+        torpilleNode.physicsBody?.isDynamic = true
+        torpilleNode.physicsBody?.categoryBitMask = torpilleCategory
+        torpilleNode.physicsBody?.contactTestBitMask = alienCategory
+        torpilleNode.physicsBody?.collisionBitMask = 0
+        torpilleNode.physicsBody?.usesPreciseCollisionDetection = true
+
+        self.addChild(torpilleNode)
+
+        let animationDuration:TimeInterval = 0.3
+
+        var actionArray = [SKAction]( )
+        actionArray.append(SKAction.move(to: CGPoint(x: player.position.x, y: self.frame.size.height + 10), duration: animationDuration))
+        actionArray.append(SKAction.removeFromParent())
+        torpilleNode?.run(SKAction.sequence(actionArray))
+        
+        
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        var firstBody:SKPhysicsBody
+        var secondBody:SKPhysicsBody
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask{
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        }else{
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        if (firstBody.categoryBitMask & torpilleCategory) != 0 &&  (secondBody.categoryBitMask & alienCategory) != 0 {
+            tropilleDidColide(torpille: firstBody.node as! SKSpriteNode, alien: secondBody.node as! SKSpriteNode)
+        }
+    }
+    
+    func tropilleDidColide(torpille:SKSpriteNode,alien:SKSpriteNode){
+        let explosion = SKEmitterNode(fileNamed: "Explosion")
+        explosion!.position = alien.position
+        self.addChild(explosion!)
+        
+        self.run(SKAction.playSoundFileNamed("explosion.mp3", waitForCompletion: false))
+        
+        torpille.removeFromParent()
+        self.run(SKAction.wait(forDuration: 2)) {
+            explosion?.removeFromParent()
+        }
+        score += 5
+    }
+    
+    
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
